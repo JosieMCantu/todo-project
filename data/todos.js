483ae8d5d404e754bdaf1ae09997dc059dbe8634
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-03-01 14:07:30 -0800

    drop tables

--- data/todos.js
+++ data/todos.js
@@ -2,16 +2,13 @@ module.exports = [
   {
     todo: 'wash the dishes',
     completed: true,
-    user_id: 1,
   },
   {
     todo: 'wash the dishes',
     completed: false,
-    user_id: 2
   },
   {
     todo: 'wash the dishes',
     completed: false,
-    user_id: 3
   }
 ];
