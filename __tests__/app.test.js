378911e2a1f4864d0e27632a37081118c9ebfc90
Josie M. Cantu <josiemcantu@Josies-MacBook-Pro.local>
2021-03-01 17:47:46 -0800

    passes all tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,57 +9,87 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token; // eslint-disable-line
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    const task = {
+      "id": 4,
+      "todo": "grocery shopping",
+      "completed": false,
+      "user_id": 2
+    };
 
-      const expectation = [
-        {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
-        },
-        {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
-        }
-      ];
+    const dataBase = {
+      ...task,
+      id: 4,
+    };
+
+    test('POST adds a new todo for a specific user', async () => {
+
+      const task = {
+        "id": 4,
+        "todo": "grocery shopping",
+        "completed": false,
+        "user_id": 2
+      };
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .post('/api/todos')
+        .send(task)
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
-        .expect(200);
+        .expect(200)
+        ;
 
-      expect(data.body).toEqual(expectation);
+      expect(data.body).toEqual(dataBase);
+    });
+
+    test('GET returns all todos for a specific user', async () => {
+
+      const data = await fakeRequest(app)
+        .get('/api/todos')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        ;
+
+      expect(data.body).toEqual(dataBase);
+    });
+
+    test('PUT updates a todo for a specific user', async () => {
+      const updatedTask = {
+        "todo": "grocery shopping",
+        "completed": true,
+      }
+
+      const data = await fakeRequest(app)
+        .put('/api/todos/4')
+        .send(updatedTask)
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200)
+        ;
+
+      expect(data.body).toEqual({ ...dataBase, completed: true });
     });
   });
 });
